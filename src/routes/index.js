56a55ec5e0d6820d8feb39c5ede5366092632e21
Kalana Hettiarachchi <kalana@orelit.com>
2022-07-17 03:40:07 +0530

    api core with prisma jwt auth

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const authRoutes = require('./auth.route');
 const userRoutes = require('./user.route');
 
 /** HEALTH CHECK */
-routes.get(`${process.env.API_VERSION_PREFIX}/`, (req, res) => res.status(200).json({ message: 'CEPEC TG service is up and running' }));
+routes.get(`${process.env.API_VERSION_PREFIX}/`, (req, res) => res.status(200).json({ message: 'service is up and running' }));
 
 routes.use(`${process.env.API_VERSION_PREFIX}/auth`, authRoutes);
 routes.use(`${process.env.API_VERSION_PREFIX}/user`, userRoutes);
