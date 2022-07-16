48b85f593f540b23f14b44f5ebe92193415134f3
Kalana Hettiarachchi <kalana@orelit.com>
2022-07-17 03:41:23 +0530

    api core with prisma jwt auth

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,7 +1,7 @@
 const asyncHandler = require('./asyncHandler.middleware');
 const validation = require('./validation.middleware');
 const checkDuplicateUser = require('./validateDuplicateUser.middleware');
-const verifyAuthToken = require('./verifyToken');
+const verifyAuthToken = require('./verifyToken.middleware');
 const authorizeUser = require('./authorizeUser.middleware');
 
 module.exports = {
