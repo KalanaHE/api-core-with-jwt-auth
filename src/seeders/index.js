2ce14f6d2816cbb0267e7289060b6759b4e228dd
Kalana Hettiarachchi <kalana@orelit.com>
2022-07-29 22:06:20 +0530

    interactive transactions seeding

--- src/seeders/index.js
+++ src/seeders/index.js
@@ -1,28 +1,32 @@
 const { PrismaClient } = require('@prisma/client');
 const userRoles = require('./data/userRoles.json');
 const userPermissions = require('./data/userPermissions.json');
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+    log: ['query', 'info', 'warn', 'error'],
+});
 
-const load = async () => {
+const seed = async () => {
     try {
-        await prisma.role.createMany({
-            data: userRoles,
-            skipDuplicates: true,
-        });
-        console.info('user roles added!');
+        await prisma.$transaction(async (prisma) => {
+            const _createdRoles = await prisma.role.createMany({
+                data: userRoles,
+                skipDuplicates: true,
+            });
+            console.info('user roles added!');
 
-        await prisma.permission.createMany({
-            data: userPermissions,
-            skipDuplicates: true,
+            if (_createdRoles) {
+                await prisma.permission.createMany({
+                    data: userPermissions,
+                    skipDuplicates: true,
+                });
+                console.info('user permissions added!');
+            }
         });
-        console.info('user permissions added!');
-
     } catch (e) {
-        console.error(e);
         process.exit(1);
     } finally {
         await prisma.$disconnect();
     }
 };
 
-load();
+seed();
